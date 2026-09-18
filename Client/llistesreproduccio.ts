32db543ff301c179bce598f04664567ac456c65c
Ayoub <achelh624@boscdelacoma.cat>
2026-09-18 17:33:29 +0200

    funcion songsplaylistartist fors dintre

--- Client/llistesreproduccio.ts
+++ Client/llistesreproduccio.ts
@@ -62,10 +62,40 @@ const users: User[] = [
 
 ]
 
+function songsSearchArtista(artist: string, songs: Song[]): Song[] {
+    return songs.filter(
+        (s: Song) => { return artist === s.artist }
+    )
+}
+
+function songsPlayListArtist(artist: string, playList: PlayList[]): Song[] {
+    let songs: Song[] = [];
+    for (let i: number = 0; i < playList.length; i++) {
+        let songsArtists: Song[] = songsSearchArtista(artist, playList[i].songs)
+        for (let i: number = 0; i < songsArtists.length, i++) {
+            songsArtists = songs.concat(songsArtists)
+        }
+
+    }
+}
+
+
+function songsArtist(artist: string, username: string, users: User[]) {
+    const user: User[] = users.filter(
+        (u: User) => { return u.name === username }
+    );
+    const PlayListUser: PlayList[] = user[0].playList;
+    return songsPlayListArtist(artist, PlayListUser);
+}
+
+
+
 console.log(users);
 
 let nameArtist: string = "MJ";
 let username: string = "Marc";
 
 const songs: Song[] = songsArtist(nameArtist, username, users);
+
+
 export { }
