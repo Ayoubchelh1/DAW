44fddce5def5be255a0e0b5b746bd407f4f17af7
Ayoub <achelh624@boscdelacoma.cat>
2026-09-23 20:58:22 +0200

    appencdChild titol

--- Client/APP/spotify/src/main.ts
+++ Client/APP/spotify/src/main.ts
@@ -1,11 +1,16 @@
 import './style.css'
 import { tracks } from './data/track'
+import { createRowSong } from './view/rowView';
 
 const appObj: HTMLElement = document.querySelector<HTMLDivElement>('#app')!;
+const titol: HTMLHeadElement = document.createElement("h1");
+titol.textContent = "Spotify";
 
 const table: HTMLTableElement = document.createElement("table");
 
 const rowSong: HTMLTableRowElement = createRowSong(tracks[0]);
+
+appObj.appendChild(titol);
 
 table.appendChild(rowSong);
 
